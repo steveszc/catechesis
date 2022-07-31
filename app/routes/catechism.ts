9a181c2b4dc5c33766f20ad3310a43d8cdb11e20
Steve Szczecina <steveszc@gmail.com>
2022-07-30 22:25:44 -0600

    feat: add basic catechism route

---
+++ app/routes/catechism.ts
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { getCatechism } from 'catechism/data';
+
+type Resolved<P> = P extends Promise<infer T> ? T : P;
+export type CatechismRouteModel = Resolved<ReturnType<CatechismRoute['model']>>;
+interface Params {
+  catechism: string;
+}
+export default class CatechismRoute extends Route {
+  model({ catechism }: Params) {
+    return getCatechism(catechism);
+  }
+}
