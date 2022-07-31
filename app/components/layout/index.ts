76c4ab8959931386ae728433de02e28ce6471923
Steve Szczecina <steveszc@gmail.com>
2022-07-31 17:26:43 -0600

    Layout + scripture/commentary/prayer

---
+++ app/components/layout/index.ts
@@ -0,0 +1,21 @@
+import templateOnlyComponent from '@ember/component/template-only';
+
+interface LayoutSignature {
+  Element: HTMLElement;
+  Args: {
+    name?: string;
+  };
+  Blocks: {
+    default: [];
+  };
+}
+
+const LayoutComponent = templateOnlyComponent<LayoutSignature>();
+
+export default LayoutComponent;
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    Layout: typeof LayoutComponent;
+  }
+}
