f326dc0153ad0725ca1096a8319af84f582205ef
Steve Szczecina <steveszc@gmail.com>
2022-07-31 20:33:33 -0600

    fix rooturl (#14)
    * fix rooturl
    
    * feat: run prod build in CI action

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/catechism';
+    ENV.rootURL = '/catechism/';
   }
 
   return ENV;
