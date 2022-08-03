22c3e6531833c362aec3e78e312a2dfc4e1091a8
Steve Szczecina <steveszc@gmail.com>
2022-08-02 20:01:46 -0600

    feat: add keachs

--- app/data/catechisms/index.ts
+++ app/data/catechisms/index.ts
@@ -1,11 +1,13 @@
 import { ForYoungChildren } from './for-young-children';
 import { Heidelberg } from './heidelberg';
+import { Keachs } from './keachs';
 import { WestminsterLonger } from './westminster-longer';
 import { WestminsterShorter } from './westminster-shorter';
 
 export const catechisms = {
   'for-young-children': ForYoungChildren,
   heidelberg: Heidelberg,
+  keachs: Keachs,
   'westminster-longer': WestminsterLonger,
   'westminster-shorter': WestminsterShorter,
 } as const;
