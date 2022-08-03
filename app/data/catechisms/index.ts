d1cbffa95ee098b1edd8a8629786a8667f76ee14
Steve Szczecina <steveszc@gmail.com>
2022-08-02 19:55:17 -0600

    feat: add Westminster Longer

--- app/data/catechisms/index.ts
+++ app/data/catechisms/index.ts
@@ -1,9 +1,11 @@
 import { ForYoungChildren } from './for-young-children';
 import { Heidelberg } from './heidelberg';
+import { WestminsterLonger } from './westminster-longer';
 import { WestminsterShorter } from './westminster-shorter';
 
 export const catechisms = {
   'for-young-children': ForYoungChildren,
   heidelberg: Heidelberg,
+  'westminster-longer': WestminsterLonger,
   'westminster-shorter': WestminsterShorter,
 } as const;
