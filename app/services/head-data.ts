54f38a1c889f7d25c56465c2fa964c2429f6c03b
Steve Szczecina <steveszc@gmail.com>
2022-08-04 22:46:14 -0600

    feat: open graph tags

--- app/services/head-data.ts
+++ app/services/head-data.ts
@@ -3,4 +3,7 @@ import { tracked } from '@glimmer/tracking';
 
 export default class HeadDataService extends Service {
   @tracked description?: string;
+  @tracked ogTitle?: string;
+  @tracked ogDescription?: string;
+  ogSite = 'Catechesis app';
 }
