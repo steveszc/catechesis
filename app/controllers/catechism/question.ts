dc0009faf72375cc978545ef6d8b3f1e3a011b05
Steve Szczecina <steveszc@gmail.com>
2022-08-08 00:11:59 -0600

    fix: alwaysShowQuestions

--- app/controllers/catechism/question.ts
+++ app/controllers/catechism/question.ts
@@ -1,11 +1,15 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 import type { QuestionRouteModel } from 'catechesis/routes/catechism/question';
+import type SettingsService from 'catechesis/services/settings';
 
 export default class QuestionController extends Controller {
   declare model: QuestionRouteModel;
+
+  @service declare settings: SettingsService;
 
   get title() {
     if (this.model.current) {
